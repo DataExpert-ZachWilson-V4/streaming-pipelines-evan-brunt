aa4f3cb6e8f73acfa2a15a2a1242f1bdf29befd0
Evan Brunt <47375706+evan-brunt@users.noreply.github.com>
2024-06-14 12:46:10 -0400

    Update session_ddl.sql

--- submission/session_ddl.sql
+++ submission/session_ddl.sql
@@ -1,17 +1,16 @@
--- Followed the homework prompt to create these columns
-
 CREATE TABLE IF NOT EXISTS {output_table} (
   session_id VARCHAR(100),
   user_id VARCHAR(100),
   session_start TIMESTAMP, 
   session_end TIMESTAMP,
-  event_count INTEGER, 
+  event_count BIGINT, 
   session_date DATE,
   city VARCHAR(100),
   state VARCHAR(100),
   country VARCHAR(100),
   operating_system VARCHAR(100),
-  browser VARCHAR(100),
+  browser_family VARCHAR(100),
   logged_in BOOLEAN
 )
 USING ICEBERG
+PARTITIONED BY (session_date)
